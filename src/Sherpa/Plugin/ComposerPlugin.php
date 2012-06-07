df4f93c7a6a7e967bff9b7b3e8694c7e1c56b80d
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-06-07 22:24:58 +0200

    Added Composer plugin

---
+++ src/Sherpa/Plugin/ComposerPlugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) 2012, Jean-Marc Fontaine
+ * All rights reserved.
+ *
+ * @package Sherpa
+ * @author Jean-Marc Fontaine <jm@jmfontaine.net>
+ * @copyright 2012 Jean-Marc Fontaine <jm@jmfontaine.net>
+ * @license http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+namespace Sherpa\Plugin;
+
+use Symfony\Component\Process\Process;
+
+class ComposerPlugin extends AbstractPlugin
+{
+    public function accept(\SplFileInfo $item)
+    {
+        if ('file' !== $item->getType()) {
+            return false;
+        }
+
+        if ($this->isInVcsDirectory($item)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function analyze(\SplFileInfo $item)
+    {
+        return array(
+            'isDefinitionFile' => 'composer.json' === $item->getFilename(),
+            'isLockFile'       => 'composer.lock' === $item->getFilename(),
+            'isPharFile'       => 'composer.phar' === $item->getFilename(),
+        );
+    }
+}
+
