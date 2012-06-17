adcee7509741a327997c3e2b2d6e2ff0c4502f43
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-06-17 19:56:27 +0200

    Removed an outdated and unused method

--- src/Sherpa/Plugin/PluginManager.php
+++ src/Sherpa/Plugin/PluginManager.php
@@ -53,53 +53,6 @@ class PluginManager
         return $this->plugins;
     }
 
-    public function process($iterator, $rootPath)
-    {
-        $data = array();
-
-        // Project data
-        $data['project'] = array(
-            'rootPath' => realpath($rootPath),
-        );
-
-        // Active plugins
-        $data['plugins'] = $this->plugins;
-
-        // Items data
-        foreach ($iterator as $item) {
-            // Item data
-            $data['items'][$item->getRelativePathname()] = array(
-                'accessTime'       => $item->getATime(),
-                'encoding'         => $item->getEncoding(),
-                'mimeType'         => $item->getMimeType(),
-                'modificationTime' => $item->getMTime(),
-                'path'             => $item->getPath(),
-                'pathname'         => $item->getPathname(),
-                'permissions'      => $item->getPerms(),
-                'relativePath'     => $item->getRelativePath(),
-                'relativePathname' => $item->getRelativePathname(),
-                'size'             => $item->getSize(),
-                'type'             => $item->getType(),
-            );
-
-            if ('link' === $item->getType()) {
-                $data['linkTarget'] = $item->getLinkTarget();
-                $data['realPath']   = $item->getRealPath();
-            }
-
-            // Plugins data
-            foreach ($this->plugins as $pluginName => $plugin) {
-                if (!$plugin->accept($item)) {
-                    continue;
-                }
-
-                $data['items'][$item->getRelativePathname()]['plugins'][$pluginName] = $plugin->analyze($item);
-            }
-        }
-
-        return $data;
-    }
-
     private function instanciatePlugin($name, array $parameters)
     {
         $normalizedName = ucwords(str_replace('_', ' ', $name));
