333ee4699979ca5f832973f6a7043cc5fae16ed4
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-06-17 18:15:52 +0200

    Added "humanize" Twig filter

--- src/Sherpa/Twig/Extension/Core.php
+++ src/Sherpa/Twig/Extension/Core.php
@@ -15,4 +15,28 @@ class Core extends \Twig_Extension
     {
         return 'sherpa_core';
     }
+
+    public function getFilters()
+    {
+        $filters = array(
+            'humanize' => new \Twig_Filter_Method($this, 'humanize'),
+        );
+
+        return $filters;
+    }
+
+    public function humanize($value)
+    {
+        switch (gettype($value)) {
+            case 'boolean':
+                $result = $value ? 'True' : 'False';
+                break;
+
+            default:
+                $result = $value;
+                break;
+        }
+
+        return $result;
+    }
 }
